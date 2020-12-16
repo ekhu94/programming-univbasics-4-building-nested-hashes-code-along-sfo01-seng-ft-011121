af04b9e723b656ef3e66407242449d2a7107f824
Erik Huang <erikhuangdrums@gmail.com>
2020-12-16 23:43:08 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,15 +1,16 @@
 def second_challenge
-  first_challenge
-  epic_tragedy[:montague] = {
-    patriarch: {},
-    matriarch: {},
-    hero: {},
-    hero_friends: []
-  }
-  epic_tragedy[:capulet] = {
-    patriarch: {},
-    matriarch: {},
-    hero: {},
-    hero_friends: []
+  epic_tragedy = {
+    montague: {
+      patriarch: {},
+      matriarch: {},
+      hero: {},
+      hero_friends: []
+    },
+    capulet: {
+      patriarch: {},
+      matriarch: {},
+      hero: {},
+      hero_friends: []
+    }
   }
 end
