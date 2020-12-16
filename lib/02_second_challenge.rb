1b0f703d896db6e8415bba111d5ea88567c2da3a
Erik Huang <erikhuangdrums@gmail.com>
2020-12-16 23:41:35 +0000

    Automatically backed up by Learn

---
+++ lib/02_second_challenge.rb
@@ -0,0 +1,15 @@
+def second_challenge
+  first_challenge
+  epic_tragedy[:montague] = {
+    patriarch: {},
+    matriarch: {},
+    hero: {},
+    hero_friends: []
+  }
+  epic_tragedy[:capulet] = {
+    patriarch: {},
+    matriarch: {},
+    hero: {},
+    hero_friends: []
+  }
+end
